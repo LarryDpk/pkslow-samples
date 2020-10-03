14618b7f63caf9db8e4150b8abb497d199bf1f54
LarryDpk <larry.dpk@gmail.com>
2020-10-03 19:22:29 +0800

    add gPRC

---
+++ grpc/src/main/java/com/pkslow/grpc/gen/OrderServiceGrpc.java
@@ -0,0 +1,288 @@
+package com.pkslow.grpc.gen;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.32.1)",
+    comments = "Source: OrderService.proto")
+public final class OrderServiceGrpc {
+
+  private OrderServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "OrderService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<com.pkslow.grpc.gen.OrderRequest,
+      com.pkslow.grpc.gen.OrderRespone> getGetOrderMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "getOrder",
+      requestType = com.pkslow.grpc.gen.OrderRequest.class,
+      responseType = com.pkslow.grpc.gen.OrderRespone.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<com.pkslow.grpc.gen.OrderRequest,
+      com.pkslow.grpc.gen.OrderRespone> getGetOrderMethod() {
+    io.grpc.MethodDescriptor<com.pkslow.grpc.gen.OrderRequest, com.pkslow.grpc.gen.OrderRespone> getGetOrderMethod;
+    if ((getGetOrderMethod = OrderServiceGrpc.getGetOrderMethod) == null) {
+      synchronized (OrderServiceGrpc.class) {
+        if ((getGetOrderMethod = OrderServiceGrpc.getGetOrderMethod) == null) {
+          OrderServiceGrpc.getGetOrderMethod = getGetOrderMethod =
+              io.grpc.MethodDescriptor.<com.pkslow.grpc.gen.OrderRequest, com.pkslow.grpc.gen.OrderRespone>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "getOrder"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.pkslow.grpc.gen.OrderRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.pkslow.grpc.gen.OrderRespone.getDefaultInstance()))
+              .setSchemaDescriptor(new OrderServiceMethodDescriptorSupplier("getOrder"))
+              .build();
+        }
+      }
+    }
+    return getGetOrderMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static OrderServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<OrderServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<OrderServiceStub>() {
+        @java.lang.Override
+        public OrderServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new OrderServiceStub(channel, callOptions);
+        }
+      };
+    return OrderServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static OrderServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<OrderServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<OrderServiceBlockingStub>() {
+        @java.lang.Override
+        public OrderServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new OrderServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return OrderServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static OrderServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<OrderServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<OrderServiceFutureStub>() {
+        @java.lang.Override
+        public OrderServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new OrderServiceFutureStub(channel, callOptions);
+        }
+      };
+    return OrderServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public static abstract class OrderServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void getOrder(com.pkslow.grpc.gen.OrderRequest request,
+        io.grpc.stub.StreamObserver<com.pkslow.grpc.gen.OrderRespone> responseObserver) {
+      asyncUnimplementedUnaryCall(getGetOrderMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getGetOrderMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.pkslow.grpc.gen.OrderRequest,
+                com.pkslow.grpc.gen.OrderRespone>(
+                  this, METHODID_GET_ORDER)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class OrderServiceStub extends io.grpc.stub.AbstractAsyncStub<OrderServiceStub> {
+    private OrderServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected OrderServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new OrderServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void getOrder(com.pkslow.grpc.gen.OrderRequest request,
+        io.grpc.stub.StreamObserver<com.pkslow.grpc.gen.OrderRespone> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getGetOrderMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class OrderServiceBlockingStub extends io.grpc.stub.AbstractBlockingStub<OrderServiceBlockingStub> {
+    private OrderServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected OrderServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new OrderServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.pkslow.grpc.gen.OrderRespone getOrder(com.pkslow.grpc.gen.OrderRequest request) {
+      return blockingUnaryCall(
+          getChannel(), getGetOrderMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class OrderServiceFutureStub extends io.grpc.stub.AbstractFutureStub<OrderServiceFutureStub> {
+    private OrderServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected OrderServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new OrderServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.pkslow.grpc.gen.OrderRespone> getOrder(
+        com.pkslow.grpc.gen.OrderRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(getGetOrderMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_GET_ORDER = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final OrderServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(OrderServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GET_ORDER:
+          serviceImpl.getOrder((com.pkslow.grpc.gen.OrderRequest) request,
+              (io.grpc.stub.StreamObserver<com.pkslow.grpc.gen.OrderRespone>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class OrderServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    OrderServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return com.pkslow.grpc.gen.OrderServiceOuterClass.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("OrderService");
+    }
+  }
+
+  private static final class OrderServiceFileDescriptorSupplier
+      extends OrderServiceBaseDescriptorSupplier {
+    OrderServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class OrderServiceMethodDescriptorSupplier
+      extends OrderServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    OrderServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (OrderServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new OrderServiceFileDescriptorSupplier())
+              .addMethod(getGetOrderMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
