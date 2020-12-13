c08519a51ed7132e11900864995c8dd5861c2c76
LarryDpk <larry.dpk@gmail.com>
2020-12-13 11:14:47 +0800

    add spring cloud gateway

---
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/pkslow/cloud/gateway/GatewayService.java
@@ -0,0 +1,11 @@
+package com.pkslow.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayService {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayService.class, args);
+    }
+}
