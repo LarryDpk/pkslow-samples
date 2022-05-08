b857aa0d679d1a365e344b4448966ed7ea2617ed
LarryDpk <larry.dpk@gmail.com>
2022-05-08 23:08:59 +0800

    spring cloud config init

---
+++ spring-cloud/spring-cloud-config/src/main/java/com/pkslow/springcloud/config/ConfigServer.java
@@ -0,0 +1,13 @@
+package com.pkslow.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
