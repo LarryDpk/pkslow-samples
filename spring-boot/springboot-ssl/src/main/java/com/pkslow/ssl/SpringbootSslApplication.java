10a1dfd2280e029de8e67016920ed6237a05c334
LarryDpk <larry.dpk@gmail.com>
2022-05-09 23:14:49 +0800

    spring boot ssl

---
+++ spring-boot/springboot-ssl/src/main/java/com/pkslow/ssl/SpringbootSslApplication.java
@@ -0,0 +1,13 @@
+package com.pkslow.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSslApplication.class, args);
+	}
+
+}
