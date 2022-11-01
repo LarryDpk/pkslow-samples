5f4709aa2f3f5c479529ae0ba2f66e56bbf4b0a8
LarryDpk <larry.dpk@gmail.com>
2022-11-01 19:23:28 +0800

    add quarkus-quickstart

---
+++ other/quarkus-quickstart/src/test/java/com/com/quarkus/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.com.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
