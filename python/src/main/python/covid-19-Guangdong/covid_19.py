66562b37196ff0654deeaf492c13c6c92d7d6150
LarryDpk <larry.dpk@gmail.com>
2022-11-30 17:46:53 +0800

    covid-19 update

--- python/src/main/python/covid-19-Guangdong/covid_19.py
+++ python/src/main/python/covid-19-Guangdong/covid_19.py
@@ -38,7 +38,7 @@ def read_data_from_csv(file_name):
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
-                # line_count += 1
+                line_count += 1
             else:
                 date = datetime.strptime(row[0], '%Y-%m-%d')
                 # 只要11月开始的数据
@@ -48,80 +48,84 @@ def read_data_from_csv(file_name):
                 data_y1.append(int(row[1]) + int(row[3]))
                 data_y2.append(int(row[2]) + int(row[4]))
                 line_count += 1
-        print(f'Read {line_count} lines.')
+        print(f'Read {line_count - 1} lines.')
 
     return data_x, data_y1, data_y2
 
 
-data_x, data_y1, data_y2 = read_data_from_csv('../beautifulsoup4-example/covid-19.csv')
-# data_x, data_y1, data_y2 = load_data()
-length = len(data_x)
-interval_show = 2
-remainder = length % interval_show
-
-fig = plt.figure(figsize=(15, 20), dpi=240)
-ax = plt.axes(xlim=(0, length + 1), ylim=(0, 10000))
-line1, = ax.plot([], [], linewidth=2.0, color='tomato')
-line2, = ax.plot([], [], linewidth=2.0, color='lightskyblue')
-
-# 为了支持中文显示
-plt.rcParams['font.family'] = ['Heiti TC']
-plt.rcParams['font.sans-serif'] = ['Heiti TC']
-plt.title('广东病例新增变化', color='dimgray', fontsize=30)
-
-text_date = ax.text(2, 9000, '', ha='left', color='black', fontsize=20)
-text_gd = ax.text(2, 8700, '广东新增', ha='left', color='red', fontsize=20)
-text_gz = ax.text(2, 8400, '广州新增', ha='left', color='blue', fontsize=20)
-
-text1 = ax.text(0, 0, '', ha='center', color='red', fontsize=10)
-text2 = ax.text(0, 0, '', ha='center', color='blue', fontsize=10)
-
-# x轴的显示
-xt = range(len(data_x) + 1)
-xt = xt[1:]
-xt_show = []
-dx_show = []
-for i, item in enumerate(data_x):
-    if (i + 1) % interval_show == remainder:
-        dx_show.append(datetime_to_str(item, '{}-{}'))
-        xt_show.append(i + 1)
-
-plt.xticks(xt_show, dx_show)
+def get_xticks_show_data(data_x):
+    # x轴的显示
+    xt = range(len(data_x) + 1)
+    xt = xt[1:]
+    x_index_show = []
+    x_show = []
+    for i, item in enumerate(data_x):
+        if (i + 1) % interval_show == remainder:
+            x_show.append(datetime_to_str(item, '{}-{}'))
+            x_index_show.append(i + 1)
+    return x_index_show, x_show
 
 
-# initialization function: plot the background of each frame
-def init():
-    line1.set_data([], [])
-    line2.set_data([], [])
-    return line1, line2, text1, text2, text_gd, text_gz, text_date
+if __name__ == '__main__':
+    data_x, data_y1, data_y2 = read_data_from_csv('../beautifulsoup4-example/covid-19.csv')
+    # data_x, data_y1, data_y2 = load_data()
+    length = len(data_x)
+    # 显示间隔，避免数据太多太密集，而看不清楚
+    interval_show = 2
+    remainder = length % interval_show
+
+    fig = plt.figure(figsize=(15, 20), dpi=240)
+    ax = plt.axes(xlim=(0, length + 1), ylim=(0, 10000))
+    line1, = ax.plot([], [], linewidth=2.0, color='tomato')
+    line2, = ax.plot([], [], linewidth=2.0, color='lightskyblue')
+
+    # 为了支持中文显示
+    plt.rcParams['font.family'] = ['Heiti TC']
+    plt.rcParams['font.sans-serif'] = ['Heiti TC']
+    plt.title('广东病例新增变化', color='dimgray', fontsize=30)
+
+    text_date = ax.text(2, 9000, '', ha='left', color='black', fontsize=20)
+    text_gd = ax.text(2, 8700, '广东新增', ha='left', color='red', fontsize=20)
+    text_gz = ax.text(2, 8400, '广州新增', ha='left', color='blue', fontsize=20)
+
+    text1 = ax.text(0, 0, '', ha='center', color='red', fontsize=10)
+    text2 = ax.text(0, 0, '', ha='center', color='blue', fontsize=10)
+
+    x_index_show, x_show = get_xticks_show_data(data_x)
+    plt.xticks(x_index_show, x_show)
 
 
-# animation function.  This is called sequentially
-def animate(i):
-    # if i == 15:
-    #     plt.axes(xlim=(0, 25), ylim=(0, 10000))
-
-    # x = dx[:i]
-    print('Processing {} for {}'.format(i, data_x[i]))
-    x = range(i + 2)
-    x = x[1:]
-    line1.set_data(x, data_y1[:i + 1])
-    line2.set_data(x, data_y2[:i + 1])
-    text1 = ax.text(i + 1, data_y1[i] + 150, data_y1[i], ha='center', color='red', fontsize=8)
-    text2 = ax.text(i + 1, data_y2[i] - 150, data_y2[i], ha='center', color='blue', fontsize=8)
-    text_gd.set_text('广东新增：' + str(data_y1[i]))
-    text_gz.set_text('广州新增：' + str(data_y2[i]))
-    text_date.set_text(datetime_to_str(data_x[i], '2022年{}月{}日'))
-    plt.scatter(x, data_y1[:i + 1], color='bisque')
-    plt.scatter(x, data_y2[:i + 1], color='cyan')
-    return line1, line2, text1, text2, text_gd, text_gz, text_date
+    # initialization function: plot the background of each frame
+    def init():
+        line1.set_data([], [])
+        line2.set_data([], [])
+        return line1, line2, text_gd, text_gz, text_date
 
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=len(data_y1), interval=1, blit=True)
+    # animation function.  This is called sequentially
+    def animate(i):
+        print('Processing {} for {}'.format(i + 1, data_x[i]))
+        x = range(i + 2)
+        x = x[1:]
+        line1.set_data(x, data_y1[:i + 1])
+        line2.set_data(x, data_y2[:i + 1])
+        if (i + 1) % interval_show == remainder or (i + 1) >= length - 5:
+            ax.text(i + 1, data_y1[i] + 150, data_y1[i], ha='center', color='red', fontsize=8)
+            ax.text(i + 1, data_y2[i] - 150, data_y2[i], ha='center', color='blue', fontsize=8)
+        text_gd.set_text('广东新增：' + str(data_y1[i]))
+        text_gz.set_text('广州新增：' + str(data_y2[i]))
+        text_date.set_text(datetime_to_str(data_x[i], '2022年{}月{}日'))
+        plt.scatter(x, data_y1[:i + 1], color='bisque')
+        plt.scatter(x, data_y2[:i + 1], color='cyan')
+        return line1, line2, text_gd, text_gz, text_date
 
-plt.rcParams['animation.ffmpeg_path'] = '/Users/larry/Software/ffmpeg/ffmpeg'
-anim.save('covid-19-Guangdong.mp4', fps=1, extra_args=['-vcodec', 'libx264'])
 
-plt.savefig('covid-19.png')
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=len(data_y1), interval=1, blit=True)
+
+    plt.rcParams['animation.ffmpeg_path'] = '/Users/larry/Software/ffmpeg/ffmpeg'
+    anim.save('covid-19-Guangdong.mp4', fps=1, extra_args=['-vcodec', 'libx264'])
+
+    # 保存图片
+    plt.savefig('covid-19.png')
