ff05316095639c6af461a064258f7ce0369adee5
LarryDpk <larry.dpk@gmail.com>
2024-05-17 08:51:24 +0800

    check all consumer groups

--- shell/kafka.sh
+++ shell/kafka.sh
@@ -3,6 +3,12 @@ bin/kafka-topics.sh --bootstrap-server host:9092 --list
 bin/kafka-consumer-groups.sh --bootstrap-server host:9092 --list
 bin/kafka-consumer-groups.sh --bootstrap-server host:9092 --describe --group groupName
 
+bin/kafka-consumer-groups.sh --bootstrap-server host:9092 --list \
+| xargs -I {} bin/kafka-consumer-groups.sh --bootstrap-server host:9092 --describe --group {} \
+> kafka.consumer-groups.info.txt
+
+
+
 $ bin/kafka-run-class.sh kafka.tools.GetOffsetShell   --broker-list localhost:9092   \
 --topic baeldung   | awk -F  ":" '{sum += $3} END {print "Result: "sum}' \
 Result: 3
