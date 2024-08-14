d97efc9e4439ef2b425ceb68f9df7c033fcedbc6
LarryDpk <larry.dpk@gmail.com>
2024-08-15 00:33:51 +0800

    kafka ssl nodejs

--- js/node-kafka-example/src/consumer.js
+++ js/node-kafka-example/src/consumer.js
@@ -1,12 +1,29 @@
 // consumer.js
 
 const { Kafka } = require('kafkajs');
+const fs = require('fs');
 
 // Initialize Kafka
-const kafka = new Kafka({
-    clientId: 'my-consumer',
-    brokers: ['localhost:9092'] // Replace with your Kafka broker addresses
-});
+let kafka = null;
+if (process.env.KAFKA_SSL_DISABLE) {
+    kafka = new Kafka({
+        clientId: 'my-consumer',
+        brokers: ['localhost:9092'] // Replace with your Kafka broker addresses
+    });
+} else {
+    kafka = new Kafka({
+        clientId: 'my-consumer',
+        brokers: ['localhost:9093'],
+        ssl: {
+            rejectUnauthorized: false, // Optionally, set to false for self-signed certs
+            ca: [fs.readFileSync('/Users/larry/IdeaProjects/pkslow-samples/other/kafka-ssl-security/ca-cert.pem', 'utf-8')],
+            key: fs.readFileSync('/Users/larry/IdeaProjects/pkslow-samples/other/kafka-ssl-security/kafka-client-key.pem', 'utf-8'),
+            cert: fs.readFileSync('/Users/larry/IdeaProjects/pkslow-samples/other/kafka-ssl-security/kafka-client-cert.pem', 'utf-8')
+            // passphrase: 'keystore-password'
+        },
+    });
+}
+
 
 // Create a consumer instance
 const consumer = kafka.consumer({ groupId: 'test-group' });
