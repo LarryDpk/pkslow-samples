fee501dd94480b923cfbd41fb14702c730eade79
LarryDpk <larry.dpk@gmail.com>
2024-10-02 23:47:48 +0800

    quickstart for openai

---
+++ python/src/main/python/quickstart-ai-api/src/openai-api.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Show me top 5 differences between Java and Python."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
