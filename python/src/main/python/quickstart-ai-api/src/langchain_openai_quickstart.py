f44ce5c087b2788bd487441732027ffa0f0e7d21
LarryDpk <larry.dpk@gmail.com>
2024-10-03 00:49:14 +0800

    langchain - prompt template example

--- python/src/main/python/quickstart-ai-api/src/langchain_openai_quickstart.py
+++ python/src/main/python/quickstart-ai-api/src/langchain_openai_quickstart.py
@@ -7,8 +7,20 @@ messages = [
     HumanMessage(content="hi, how are you?"),
 ]
 result = model.invoke(messages)
-
 print(result.content)
 
+# prompt template example
+from langchain_core.prompts import ChatPromptTemplate
 
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+result_template = prompt_template.invoke({"language": "italian", "text": "hi, how are you?"})
+print(result_template)
+result_template = prompt_template.invoke({"language": "Chinese", "text": "hi, how are you?"})
+print(result_template)
+
+result = model.invoke(result_template)
+print(result.content)
 
